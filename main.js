152b08b501dc148af2b30d4d1424b3290c785224
ljoseck <Louis.joseck@ttu.edu>
2019-03-04 18:23:22 -0600

    click and drag

--- main.js
+++ main.js
@@ -5,11 +5,39 @@ var context = canvas.getContext("2d");
 
 startUp();
 
+var startX;
+var startY;
+
+function onMouseDownCanvas(){
+	//alert("h");
+	startX = event.offsetX;
+	startY = event.offsetY;
+
+	context.beginPath();
+	context.arc(startX, startY, 4, 0, 2 * Math.PI);
+	context.stroke();
+	
+}
+
+function onMouseUpCanvas(){
+	//alert("h");
+	var endX = event.offsetX;
+	var endY = event.offsetY;
+
+	context.beginPath();
+	context.moveTo(startX, startY);
+	context.lineTo(endX, endY);
+	context.stroke();
+	
+}
 
 function startUp(){
 	console.log(convasDiv.offsetHight);
 	canvas.width = convasDiv.offsetWidth;
 	canvas.hight = convasDiv.offsetHight;
+	canvas.addEventListener("mousedown", onMouseDownCanvas);
+	canvas.addEventListener("mouseup", onMouseUpCanvas);
+	//canvas.onClick = onClickCanvas;
 	
 	
 }
