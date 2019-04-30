2ee0ee32a8ff8e6aa3de583ab0284b86d3e73c3f
Chase G <chaseagrossman@gmail.com>
2019-04-29 19:24:14 -0500

    Another

--- Attributes/Attributes_size.js
+++ Attributes/Attributes_size.js
@@ -3,7 +3,7 @@
 function autoFill(width, height, x, y, pictureID) {
 	var heightfeet = height/40;
 	var widthfeet = width/40;
-	document.getElementById('inputWidth').value = widthfeet
-	document.getElementById('inputLength').value = heightfeet
+	document.getElementById('inputWidth').value = widthfeet;
+	document.getElementById('inputLength').value = heightfeet;
 }
 
