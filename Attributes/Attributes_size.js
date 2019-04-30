46ba6edcb98245be55493d8c2f68ed91f740ffcc
Chase G <chaseagrossman@gmail.com>
2019-04-29 19:23:43 -0500

    Fixing JS file

---
+++ Attributes/Attributes_size.js
@@ -0,0 +1,9 @@
+
+
+function autoFill(width, height, x, y, pictureID) {
+	var heightfeet = height/40;
+	var widthfeet = width/40;
+	document.getElementById('inputWidth').value = widthfeet
+	document.getElementById('inputLength').value = heightfeet
+}
+
